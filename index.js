f9765a0299b965c5fd30d5e151b5a7b502afece8
Moinuddin Kamal <IT@pronayonbd.com>
2021-11-14 11:59:14 +0600

    update code

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const ObjectId = require("mongodb").ObjectId;
 const port = process.env.PORT || 8000;
 
 // const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.texip.mongodb.net/randomUsers?retryWrites=true&w=majority`;
-const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4t39k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri =`mongodb+srv://kamal:kamal12345@cluster0.4t39k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
